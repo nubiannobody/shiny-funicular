34f74ce85e31bc7e683fd2825e38c86183ad2d4e
Kween <kweenyoallhiada@gmail.com>
2023-01-19 22:30:49 -0800

    Exercise complete

--- hiFunction.js
+++ hiFunction.js
@@ -4,7 +4,7 @@ let greeting = "Hi there"
 // Create a function that logs out "Hi there, Linda!" when called
 
 function hiLinda() {
-    console.log(greeting + "," + " " + firstName + "!");
+    console.log(greeting + ", " + firstName + "!");
 }
 
 hiLinda();
